f6a19ac55ce6890444b7ce23bd0086edbf5841fa
윤성 <yskimno1@gmail.com>
2019-03-07 20:29:56 +0900

    ?

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -245,8 +245,8 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
-  // list_insert_ordered(&ready_list, &t->elem, compare_priority, 0);
+  // list_push_back (&ready_list, &t->elem);
+  list_insert_ordered(&ready_list, &t->elem, compare_priority, 0);
   
 
   t->status = THREAD_READY;
@@ -317,7 +317,8 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (curr != idle_thread) 
-    list_push_back (&ready_list, &curr->elem);
+    list_insert_ordered(&sleep_list, &curr->elem, compare_wakeup_time, 0);
+    // list_push_back (&ready_list, &curr->elem);
   curr->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
