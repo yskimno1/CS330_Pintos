416dbc953a8aa9b3e8f82b5a20f1a5cad08980dd
윤성 <yskimno1@gmail.com>
2019-03-08 13:03:50 +0900

    sema_down bug fixed

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -69,7 +69,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_insert_ordered(&sema->waiters, &thread_current, compare_priority, 0);
+      list_insert_ordered(&sema->waiters, &thread_current()->elem, compare_priority, 0);
       // list_push_back (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
